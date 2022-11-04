6e8123db9f8530ecdff68c026a4a0b669d5a5cb9
MarBozzz <mbozzuto@libero.it>
2022-11-04 17:21:13 +0100

    [added] VueJs part

--- js/script.js
+++ js/script.js
@@ -10,7 +10,57 @@ Ogni todo sarà un oggetto, formato da due proprietà:
 4. al click della croce eliminare il task se fatto, altrimenti scrivere un messaggio di errore
 5. dopo aver scritto il task con invio o al click del bottone aggiungere il nuovo task (come non fatto) in cima all’elenco dei task
 6. verificare la lunghezza minima del nuovo task
-7. se l’elenco è vuoto non mostrare la lista ma mostrare il messaggio “Non ci sono task”BONUS
+7. se l’elenco è vuoto non mostrare la lista ma mostrare il messaggio “Non ci sono task”
+BONUS
 - Verificare se il task è già presente e nel caso mostrare l’errore
 - ... il limite è la fantasia ;-)
 */
+
+
+const {createApp} = Vue;
+
+createApp ({
+  data(){
+    return {
+      errorMessage: '',
+      newMessage: '',
+      tasks : [
+        {
+          text : 'Spostare gli equilibri',
+          done : true
+        },
+        {
+          text : 'Fare gavettoni d\'inverno',
+          done : false
+        },
+        {
+          text : 'Andare ai rave',
+          done : false
+        }
+      ]
+    }
+  },
+  methods:{
+    removeTask(index){
+      this.errorMessage = '';
+      if(this.tasks[index].done){
+        this.tasks.splice(index,1)
+      } else {
+        this.errorMessage = 'Non fare il furbo, devi ancora completare il task che volevi cancellare'
+      }
+    },
+    addTask() {
+      this.errorMessage = '';
+      if (this.newMessage.length < 5){
+        this.errorMessage = 'Dai su, un po\' di impegno a mettere insieme almeno 5 lettere...'
+      } else {
+        const newTask = {
+          text : this.newMessage,
+          done : false
+        }
+        this.tasks.unshift(newTask);
+        this.newMessage = ''
+      }
+    }
+  }
+}).mount('#app')
